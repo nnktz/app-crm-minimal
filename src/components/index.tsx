a0dccd663b8fdad9e371bfdaccbc7e137693c9d9
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2024-01-22 14:28:32 +0700

    dashboard total count card

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import DealChart from './home/deals-chart';
 import UpcomingEvents from './home/upcoming-events';
 import UpcomingEventsSkeleton from './skeleton/upcoming-events';
+import TotalCountCard from './home/total-count-card';
 
-export { DealChart, UpcomingEvents, UpcomingEventsSkeleton };
+export { DealChart, UpcomingEvents, UpcomingEventsSkeleton, TotalCountCard };
